ec2a0adafafb42bc07c02009a53bd5390b6b3e4f
lusmoura <luisa@cohere.com>
2025-03-19 18:18:31 +0000

    aula 2 - exercicios aula

--- Aula-2/exercicios_aula/1_variaveis.py
+++ Aula-2/exercicios_aula/1_variaveis.py
@@ -16,8 +16,36 @@ print(booleano)
 
 # list - lista
 lista = [1, 2, 3, 4, 5]
+primeiro_elemento = lista[0]
+ultimo_elemento = lista[-1]
 print(lista)
+print(primeiro_elemento)
+print(ultimo_elemento)
 
 # dict - dicionário
 dicionario = {"nome": "João", "idade": 20}
+nome = dicionario["nome"]
+idade = dicionario["idade"]
 print(dicionario)
+print(nome)
+print(idade)
+
+# input
+nome = input("Digite seu nome: ")
+idade = input("Digite a sua idade:")
+frase = f"Seu nome é: {nome}. Sua idade é {idade}"
+print(frase)
+
+# lista de dicionarios
+post_1 = {
+    "titulo": "Programação em Python",
+    "resumo": "Programar em Python é muito top" 
+}
+
+post_2 = {
+    "titulo": "Variaveis",
+    "resumo": "Variáveis são caixinhas que armazenam valores. Existem diversos tipos de variáveis. Isso, por exemplo, é uma string em um dicionário"
+}
+
+lista_de_posts = [post_1, post_2]
+print(lista_de_posts)
